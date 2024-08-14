112fc0e428d3936cd97b029e2d837b665b56e0e5
shravya462 <shravyapoojary462@gmail.com>
2024-08-14 15:50:42 +0530

    removed screen util

--- lib/presentation/message/chat.dart
+++ lib/presentation/message/chat.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_project/res/assets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -17,19 +15,19 @@ class _ChatScreenState extends State<ChatScreen> {
   List<Message> messages = [
     Message(
       text:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
       isSentByMe: false,
       time: '1:30 PM',
     ),
     Message(
       text:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
       isSentByMe: true,
       time: '1:30 PM',
     ),
     Message(
       text:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the text ever since the 1500s, when an unknown.",
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the text ever since the 1500s, when an unknown.",
       isSentByMe: false,
       time: '1:30 PM',
     ),
@@ -62,7 +60,7 @@ class _ChatScreenState extends State<ChatScreen> {
             child: ClipRRect(
               borderRadius: BorderRadius.circular(100),
               child: Image.asset('assets/images/girl2.png',
-                  height: 44.h, width: 44.w, fit: BoxFit.cover),
+                  height: 44, width: 44, fit: BoxFit.cover),
             ),
           ),
           title: Align(
@@ -71,18 +69,18 @@ class _ChatScreenState extends State<ChatScreen> {
               onSelected: (value) =>
                   _handlePopUpItem(value.toString(), context),
               constraints: BoxConstraints(
-                maxWidth: 145.w,
+                maxWidth: 145,
               ),
               itemBuilder: (BuildContext context) {
                 return {"Block", 'Report', 'Remove'}.map((String choice) {
                   return PopupMenuItem(
                     value:
-                    choice.toLowerCase(), // Correct usage of toLowerCase()
+                        choice.toLowerCase(), // Correct usage of toLowerCase()
                     child: Container(
-                      height: 40.h,
-                      width: 125.w,
+                      height: 40,
+                      width: 125,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(4.r),
+                        borderRadius: BorderRadius.circular(4),
                         color: Colors.blue.withOpacity(0.3),
                       ),
                       child: Padding(
@@ -92,7 +90,7 @@ class _ChatScreenState extends State<ChatScreen> {
                           choice,
                           style: TextStyle(
                             color: Colors.blue,
-                            fontSize: 14.sp,
+                            fontSize: 14,
                             fontWeight: FontWeight.w400,
                           ),
                         ),
@@ -121,7 +119,7 @@ class _ChatScreenState extends State<ChatScreen> {
                           horizontal: 10, vertical: 10),
                       child: InkWell(
                         onTap: () {
-                         /* Get.to(MycontractScreen());*/
+                          /* Get.to(MycontractScreen());*/
                         },
                         child: Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -131,24 +129,25 @@ class _ChatScreenState extends State<ChatScreen> {
                                 : CrossAxisAlignment.start,
                             children: [
                               Container(
-                                constraints: BoxConstraints(maxWidth: 200.w),
+                                constraints: BoxConstraints(maxWidth: 200),
                                 padding: const EdgeInsets.symmetric(
                                     horizontal: 10, vertical: 10),
                                 decoration: BoxDecoration(
                                   color: Colors.blue,
                                   borderRadius: message.isSentByMe
                                       ? BorderRadius.only(
-                                    topLeft: Radius.circular(15.r),
-                                    topRight: Radius.circular(15.r),
-                                    bottomLeft: Radius.circular(15.r),
-                                  )
+                                          topLeft: Radius.circular(15),
+                                          topRight: Radius.circular(15),
+                                          bottomLeft: Radius.circular(15),
+                                        )
                                       : BorderRadius.only(
-                                    topLeft: Radius.circular(15.r),
-                                    topRight: Radius.circular(15.r),
-                                    bottomRight: Radius.circular(15.r),
-                                  ),
+                                          topLeft: Radius.circular(15),
+                                          topRight: Radius.circular(15),
+                                          bottomRight: Radius.circular(15),
+                                        ),
                                 ),
-                                child: Text('hi'),/*TextWidget(
+                                child: Text(
+                                    'hi'), /*TextWidget(
                                   text: message.text,
                                   color: Colors.white,
                                   size: 12.sp,
@@ -156,9 +155,10 @@ class _ChatScreenState extends State<ChatScreen> {
                                 ),*/
                               ),
                               SizedBox(
-                                  height: 5
-                                      .h), // Adjusts spacing between message and time
-                              Text('hi'),/*TextWidget(
+                                  height:
+                                      5), // Adjusts spacing between message and time
+                              Text(
+                                  'hi'), /*TextWidget(
                                 text: message.time,
                                 color: Colors.black.withOpacity(0.7),
                                 size: 10.sp,
@@ -183,11 +183,11 @@ class _ChatScreenState extends State<ChatScreen> {
                     },
                     child: Container(
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30.r),
+                        borderRadius: BorderRadius.circular(30),
                         color: const Color(0xffF1F1F1),
                       ),
-                      height: 50.h,
-                      width: 234.w,
+                      height: 50,
+                      width: 234,
                       child: TextFormField(
                         controller: messageController,
                         decoration: InputDecoration(
@@ -196,43 +196,43 @@ class _ChatScreenState extends State<ChatScreen> {
                             padding: const EdgeInsets.all(10.0),
                             child: SvgPicture.asset(
                               Assets.person,
-                              height: 18.h,
-                              width: 18.w,
+                              height: 18,
+                              width: 18,
                               fit: BoxFit.contain,
                             ),
                           ),
                           hintText: 'Type Something....',
                           hintStyle: TextStyle(
-                            fontSize: 13.sp,
+                            fontSize: 13,
                             fontWeight: FontWeight.w400,
                             color: Colors.grey,
                           ),
                           contentPadding: const EdgeInsets.symmetric(
                               vertical: 10, horizontal: 16),
                           border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30.r),
+                            borderRadius: BorderRadius.circular(30),
                           ),
                         ),
                       ),
                     ),
                   ),
                   Container(
-                    height: 50.h,
-                    width: 71.w,
+                    height: 50,
+                    width: 71,
                     child: IconButton(
                       onPressed: () {
                         _sendMessage();
                       },
                       icon: Icon(
                         Icons.arrow_forward_ios,
-                        size: 15.sp,
+                        size: 15,
                         color: const Color(0xffB9B9B9),
                       ),
                     ),
                     decoration: BoxDecoration(
                       color: const Color(0xffF1F1F1),
                       border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(30.r),
+                      borderRadius: BorderRadius.circular(30),
                     ),
                   )
                 ],
@@ -262,18 +262,19 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 
   void _showDialogBox(
-      BuildContext context,
-      String text,
-      String text1,
-      String btntext1,
-      String btntext2,
-      ) {
+    BuildContext context,
+    String text,
+    String text1,
+    String btntext1,
+    String btntext2,
+  ) {
     showDialog(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
               title: Center(
-                child: Text('hi'),/*TextWidget(
+                child: Text(
+                    'hi'), /*TextWidget(
                   text: 'Are you Sure?',
                   color: Colors.blue,
                   size: 20.sp,
@@ -285,7 +286,7 @@ class _ChatScreenState extends State<ChatScreen> {
                   text: text,
                   style: TextStyle(
                     fontWeight: FontWeight.w500,
-                    fontSize: 15.sp,
+                    fontSize: 15,
                     color: Colors.black,
                   ),
                   children: <TextSpan>[
@@ -294,7 +295,7 @@ class _ChatScreenState extends State<ChatScreen> {
 
                       text: text1,
                       style: TextStyle(
-                        fontSize: 15.sp,
+                        fontSize: 15,
                         fontWeight: FontWeight.w700,
                         color: Colors.black,
                       ),
@@ -321,9 +322,10 @@ class _ChatScreenState extends State<ChatScreen> {
                             side: BorderSide(color: Colors.lightBlue),
                           )),
                       onPressed: () {
-                      /*  Get.back();*/
+                        /*  Get.back();*/
                       },
-                      child: Text('hi'),/*TextWidget(
+                      child: Text(
+                          'hi'), /*TextWidget(
                         text: btntext1,
                         color: Colors.white,
                         size: 12.sp,
@@ -331,7 +333,7 @@ class _ChatScreenState extends State<ChatScreen> {
                       ),*/
                     ),
                     SizedBox(
-                      width: 10.w,
+                      width: 10,
                     ),
                     TextButton(
                       style: TextButton.styleFrom(
@@ -341,11 +343,12 @@ class _ChatScreenState extends State<ChatScreen> {
                             borderRadius: BorderRadius.circular(100),
                           )),
                       onPressed: () {
-                      /*  Get.back();*/
+                        /*  Get.back();*/
                       },
                       child: Padding(
                         padding: const EdgeInsets.all(4.0),
-                        child: Text('hi'),/*TextWidget(
+                        child: Text(
+                            'hi'), /*TextWidget(
                           text: btntext2,
                           color: Colors.black,
                           size: 12.sp,
