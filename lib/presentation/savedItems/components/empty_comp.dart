91e153400e8fb71388460f65c975b7ce95c38bc3
shravya462 <shravyapoojary462@gmail.com>
2024-08-14 14:18:38 +0530

    not done

---
+++ lib/presentation/savedItems/components/empty_comp.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EmptyComp extends StatelessWidget {
+  const EmptyComp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/presentation/savedItems/savedItem_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project/presentation/savedItems/components/empty_comp.dart';
+
+class SaveditemScreen extends StatefulWidget {
+  const SaveditemScreen({super.key});
+
+  @override
+  State<SaveditemScreen> createState() => _SaveditemScreenState();
+}
+
+class _SaveditemScreenState extends State<SaveditemScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: EmptyComp(),
+    );
+  }
+}
